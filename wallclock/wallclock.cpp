ce51c58bd3d10aa840907f4044b6ffcbf70ff74f
Michael Farrell <micolous+git@gmail.com>
2019-12-11 16:47:51 +1100

    stop hard coding

--- wallclock/wallclock.cpp
+++ wallclock/wallclock.cpp
@@ -1,6 +1,6 @@
 /*
 wallclock/wallclock.cpp
-Copyright 2013-2015 Michael Farrell <http://micolous.id.au/>
+Copyright 2013-2015, 2019 Michael Farrell <http://micolous.id.au/>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -27,6 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <unicode/locid.h>
 #include <unicode/smpdtfmt.h>
 #include <unicode/timezone.h>
+#include <unicode/udat.h>
 #include <unicode/unistr.h>
 #include <unicode/ustdio.h>
 
@@ -38,12 +39,17 @@ extern "C" {
 }
 #endif
 
+using icu::Calendar;
+using icu::SimpleDateFormat;
+using icu::TimeZone;
+using icu::UnicodeString;
+
 static tm1640_display* display;
-static const TimeZone *tzPacific, *tzZurich, *tzTokyo, *tzSydney;
-static SimpleDateFormat *dfPacific, *dfZurich, *dfTokyo, *dfSydney;
+static const TimeZone *tzA, *tzB, *tzC, *tzD;
+static SimpleDateFormat *dfA, *dfB, *dfC, *dfD;
 
 void update_current_time(int mode) {
-	UnicodeString uPacific, uZurich, uTokyo, uSydney;
+	UnicodeString uA, uB, uC, uD;
 	UErrorCode success = U_ZERO_ERROR;
 	Calendar *calendar = Calendar::createInstance(success);
 	UDate curDate = calendar->getNow();// * 1000;
@@ -51,17 +57,17 @@ void update_current_time(int mode) {
 	char buf[33];
 	int size = mode == 0 ? 5 : 4;
 
-	dfPacific->format(curDate, uPacific);
-	dfZurich->format(curDate, uZurich);
-	dfTokyo->format(curDate, uTokyo);
-	dfSydney->format(curDate, uSydney);
+	dfA->format(curDate, uA);
+	dfB->format(curDate, uB);
+	dfC->format(curDate, uC);
+	dfD->format(curDate, uD);
 
 	memset(buf, 0, sizeof(buf));
 	int offset = mode * size + (mode > 0 ? 1 : 0);
-	uPacific.extract(offset, size, buf);
-	uZurich.extract(offset, size, buf+size);
-	uTokyo.extract(offset, size, buf+(size*2));
-	uSydney.extract(offset, size, buf+(size*3));
+	uA.extract(offset, size, buf);
+	uB.extract(offset, size, buf+size);
+	uC.extract(offset, size, buf+(size*2));
+	uD.extract(offset, size, buf+(size*3));
 
 	//printf("screen = %s\n", buf);
 	tm1640_displayWrite(display, 0, buf, size*4, 0);
@@ -81,34 +87,57 @@ void loop() {
 		sleep(1);
 	}
 }
-	
+
+const TimeZone* getTimeZoneOrDie(const char* id) {
+	const TimeZone* tz = TimeZone::createTimeZone(id);
+	if (TimeZone::getUnknown() == *tz) {
+		fprintf(stderr, "Timezone `%s` is unknown.\n", id);
+		return NULL;
+	}
+	return tz;
+}
+
 int main(int argc, char** argv) {
 	UErrorCode success = U_ZERO_ERROR;
 
+	if (argc != 5) {
+		fprintf(stderr, "must specify exactly 4 timezone identifiers:\n");
+		fprintf(stderr, "%s 'Australia/Perth' 'Australia/Adelaide' 'Australia/Brisbane' 'Australia/Sydney'\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	// load tzdata
+	if ((tzA = getTimeZoneOrDie(argv[1])) == NULL) {
+		return EXIT_FAILURE;
+	}
+	if ((tzB = getTimeZoneOrDie(argv[2])) == NULL) {
+		return EXIT_FAILURE;
+	}
+	if ((tzC = getTimeZoneOrDie(argv[3])) == NULL) {
+		return EXIT_FAILURE;
+	}
+	if ((tzD = getTimeZoneOrDie(argv[4])) == NULL) {
+		return EXIT_FAILURE;
+	}
+
+	dfA = new SimpleDateFormat(UnicodeString("HH.mmHHmmEEE "), success);
+	dfA->setTimeZone(*tzA);
+	dfB = (SimpleDateFormat*)dfA->clone();
+	dfB->setTimeZone(*tzB);
+	dfC = (SimpleDateFormat*)dfA->clone();
+	dfC->setTimeZone(*tzC);
+	dfD = (SimpleDateFormat*)dfA->clone();
+	dfD->setTimeZone(*tzD);
+
 	// start tm1640
-	display = tm1640_init(1, 0);
+	display = tm1640_init(SCLK_PIN, DIN_PIN);
 	if (display == NULL) {
 		fprintf(stderr, "error initialising display\n");
 		return EXIT_FAILURE;
 	}
-	
+
 	tm1640_displayOn(display, 7);
 	tm1640_displayClear(display);
-
-	// load tzdata
-	tzPacific = TimeZone::createTimeZone("America/Los_Angeles");
-	tzZurich = TimeZone::createTimeZone("Europe/Zurich");
-	tzTokyo = TimeZone::createTimeZone("Asia/Tokyo");
-	tzSydney = TimeZone::createTimeZone("Australia/Sydney");
-
-	dfPacific = new SimpleDateFormat(UnicodeString("HH.mmHHmmEEE "), success);
-	dfPacific->setTimeZone(*tzPacific);
-	dfZurich = (SimpleDateFormat*)dfPacific->clone();
-	dfZurich->setTimeZone(*tzZurich);
-	dfTokyo = (SimpleDateFormat*)dfPacific->clone();
-	dfTokyo->setTimeZone(*tzTokyo);
-	dfSydney = (SimpleDateFormat*)dfPacific->clone();
-	dfSydney->setTimeZone(*tzSydney);
 
 	// run main loop
 	loop();
